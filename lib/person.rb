7f5054cfd40e094c28fbe44cada83727883ff9e7
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-30 20:36:19 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,16 +1,11 @@
 class Person
- end
-
- def talk
+  def talk
    puts "Hello World!"
  end
+end
 
+class Person
 def walk
    puts "The Person is walking"
  end
-
-adele_goldberg = Person.new
-puts adele_goldberg
-
-alan_kay = Person.new
-puts alan_kay
+end
